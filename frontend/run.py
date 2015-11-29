2acc88b6e45aac4d749ed207099e0450ff222ccc
Reginaldo Junior <76regi@gmail.com>
2015-11-29 20:07:00 -0200

    create new sites by form

--- frontend/run.py
+++ frontend/run.py
@@ -1,9 +1,11 @@
 #-*- coding: utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from pymongo import MongoClient
 
 app = Flask(__name__)
+
+app.debug = True
 
 client = MongoClient('localhost', 27017)
 db = client['searchengine']
@@ -12,6 +14,17 @@ db = client['searchengine']
 def index():
     sites = db['sites'].find()
     return render_template('index.html', sites=sites)
+
+@app.route("/newsite", methods=["POST"])
+def newsite():
+    data = {
+        'url': request.form['site']
+    }
+
+    if db['sites'].insert(data):
+        return redirect('/')
+
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run()
